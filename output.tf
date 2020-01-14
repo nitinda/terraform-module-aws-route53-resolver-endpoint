20a764d001704154191f4b1cf9cbcf32955824a5
nitin-das <nitin.das@thomson.co.uk>
2020-01-14 17:01:41 +0000

    Modified code

--- output.tf
+++ output.tf
@@ -1,11 +1,12 @@
 output "id" {
-  value = "${aws_route53_resolver_endpoint.route53_resolver_endpoint.id}"
+  value = aws_route53_resolver_endpoint.route53_resolver_endpoint.id
 }
 
 output "arn" {
-  value = "${aws_route53_resolver_endpoint.route53_resolver_endpoint.arn}"
+  value = aws_route53_resolver_endpoint.route53_resolver_endpoint.arn
 }
 
 output "host_vpc_id" {
-  value = "${aws_route53_resolver_endpoint.route53_resolver_endpoint.host_vpc_id}"
+  value = aws_route53_resolver_endpoint.route53_resolver_endpoint.host_vpc_id
 }
+
--- route53-resolver-endpoint.tf
+++ route53-resolver-endpoint.tf
@@ -1,7 +1,13 @@
 resource "aws_route53_resolver_endpoint" "route53_resolver_endpoint" {
-  name               = "${var.name}"
-  direction          = "${var.direction}"
-  security_group_ids = "${var.security_group_ids}"
-  ip_address         = "${var.ip_address}"
-  tags               = "${var.tags}"
+  name               = var.name
+  direction          = var.direction
+  security_group_ids = var.security_group_ids
+  dynamic "ip_address" {
+    for_each = length(var.ip_address) == 0 ? [] : var.ip_address
+    content {
+      ip        = lookup(ip_address.value, "ip", null)
+      subnet_id = ip_address.value.subnet_id
+    }
+  }
+  tags = var.tags
 }
